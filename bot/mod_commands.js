712967ac3f1747afd354361cf8417e31b72dc1b9
Evan Boudreau <xanizl@me.com>
2016-01-20 18:00:33 -0500

    Tried to add an update command to update the bot remotely but it doesn't work so is commented out

--- bot/mod_commands.js
+++ bot/mod_commands.js
@@ -192,7 +192,53 @@ var mod_commands = {
 					bot.reply(msg,"using the delete command requires a interger between 1-500")
 			}
 		}
-	},
+	}/*,
+	"update": {
+		usage: "[none]",
+		description:"Updates the bot to the most current veresion on github",
+		process: function(bot, msg)
+		{
+			if (msg.author.id === "87600987040120832" && options.github)
+			{
+				bot.sendMessage(msg.channel,"fetching updates...",function(error,sentMsg){
+						console.log("updating...");
+					var spawn = require('child_process').spawn;
+						var log = function(err,stdout,stderr){
+								if(stdout){console.log(stdout);}
+								if(stderr){console.log(stderr);}
+						};
+						var fetch = spawn('git', ['fetch']);
+						fetch.stdout.on('data',function(data){
+								console.log(data.toString());
+						});
+						fetch.on("close",function(code){
+								var reset = spawn('git', ['reset','--hard','origin/master']);
+								reset.stdout.on('data',function(data){
+										console.log(data.toString());
+								});
+								reset.on("close",function(code){
+										var npm = spawn('npm', ['install']);
+										npm.stdout.on('data',function(data){
+												console.log(data.toString());
+										});
+										npm.on("close",function(code){
+												console.log("goodbye");
+												bot.sendMessage(msg.channel,"brb!",function(){
+														bot.logout(function(){
+																process.exit();
+														});
+												});
+										});
+								});
+						});
+				});
+			}
+			else
+			{
+
+			}
+		}
+	}*/
 	/*,
 	"eval": {
 		usage: "[command]",
