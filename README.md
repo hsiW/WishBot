a9e33ab3bc395b3085851feee739d3a9599e985c
Evan Boudreau <xanizl@me.com>
2016-01-23 18:40:15 -0500

    make it better

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wishbot
-A Discord bot made by Wish written with the Discord js API
--- WishBot.js
+++ WishBot.js
@@ -11,7 +11,15 @@ var cleverbot = require("cleverbot-node");
 var onee = new cleverbot;
 cleverbot.prepare(function () {});
 
-var servers = getServers();
+var chalk = require("chalk");
+var c = new chalk.constructor({enabled: true});
+
+var serverC = c.black.bold;
+var channelC = c.green.bold;
+var userC = c.cyan.bold;
+var warningC = c.yellow.bold;
+var errorC = c.red.bold;
+var botC = c.magenta.bold;
 
 var lastMessage;//used to store the last message for use in spam detection
 var lastAuthor;//used to store the last author for use in spam detection
@@ -19,117 +27,72 @@ var commandsProcessed = 0;//used to count the ammount of commands processed in t
 var talked = 0;//used to count how many times people talked to WishBot in the current session
 var rand = 0;//used for randomly pikcing game later on in code
 
-
 //Does this stuff when the bot is ready and running
 bot.on("ready", function ()
 {
-	checkServers();//checks if servers changed
 	bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))]);//randomly sets a game
-	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");//tells you that the bot is ready as well as in how many channels
+	console.log(botC("@WishBot")+" - Ready to begin! Serving in " + channelC(bot.channels.length) + " channels");//tells you that the bot is ready as well as in how many channels
 });
 
 //Does this stuff when the bot detects a message, can be in a channel its part of or through a private chat
 bot.on("message", function (msg) {
-	//Stuff to randomly change the game the bot is playing
-	rand = Math.floor((Math.random() * 33) + 1); //Randomly Generates a number and then checks that number to see if it is 1 and if it is the game is randomly changed
-	if (rand == 1) {
-		var randgame = games[Math.floor(Math.random() * (games.length))];
-		bot.setPlayingGame(randgame);
-		console.log("@WishBot - Randomly changed game to \"" + randgame + "\"");
-	}
 	//Start of command checking
-	if ((!(msg.channel.isPrivate)) && ((msg.content[0] === options.command_prefix) || (msg.content[0] === options.mod_command_prefix)) && (msg.author.id != bot.user.id)) {
+	var suffix = msg.content.substring((msg.content.split(" ")[0].substring(1)).length + 2);
+	if (!(msg.channel.isPrivate) && ((msg.content[0] === options.command_prefix) || (msg.content[0] === options.mod_command_prefix)) && (msg.author.id != bot.user.id)) {
 		var cmdTxt = msg.content.split(" ")[0].substring(1);
-		var suffix = msg.content.substring(cmdTxt.length + 2);
-		if (msg.content[0] === options.command_prefix && commands[cmdTxt]) {
-			var cmd = commands[cmdTxt];
+		if ((commands[cmdTxt] && msg.content[0] === options.command_prefix) || (mod_commands[cmdTxt] && msg.content[0] === options.mod_command_prefix && msg.channel.permissionsOf(msg.sender).hasPermission("manageRoles")))
+		{
 			commandsProcessed = commandsProcessed + 1;
-			console.log("#" + msg.channel.name + ": @WishBot - " + cmdTxt + " was used by " + msg.author.username);
-			cmd.process(bot, msg, suffix);
-		}
-		if ((msg.content[0] === options.mod_command_prefix) && (msg.channel.permissionsOf(msg.sender)
-				.hasPermission("manageServer")) && mod_commands[cmdTxt]) {
-			var cmd = mod_commands[cmdTxt];
-			commandsProcessed = commandsProcessed + 1;
-			console.log("#" + msg.channel.name + ": @WishBot - " + cmdTxt + " was used by " + msg.author.username);
+			if (commands[cmdTxt] && msg.content[0] === options.command_prefix){var cmd = commands[cmdTxt]}
+			if (mod_commands[cmdTxt] && msg.content[0] === options.mod_command_prefix){var cmd = mod_commands[cmdTxt]}
+			console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": "+botC("@WishBot")+" - "+warningC(cmdTxt) + " was used by " + userC(msg.author.username));
+			bot.sendMessage("@"+msg.channel.server.name+": #" + msg.channel.name + ": @WishBot - `" + cmdTxt + "` was used by " + msg.author.username);
 			cmd.process(bot, msg, suffix, commandsProcessed, talked);
+			if(cmd.delete){bot.deleteMessage(msg)}
 		}
 	}
 	if (!(msg.channel.isPrivate) && (msg.content.indexOf(bot.user.mention()) == 0) && (msg.author.id != bot.user.id)) {
-		console.log("#" + msg.channel.name + ": " + msg.author.username + " - " + msg.content);
-		var suffix = msg.content.substring((msg.content.split(" ")[0].substring(1)).length + 2);
+		console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content);
 		var conv = suffix.split(" ");
 		bot.startTyping(msg.channel);
 		talked = talked + 1;
 		onee.write(conv, function (response) {
-			console.log("#" + msg.channel.name + ": @WishBot - 🌐 - " + response.message);
+			console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": "+botC("@WishBot")+" - 🌐 - " + response.message);
 			bot.sendMessage(msg.channel, "🌐 - " + response.message);
 			bot.stopTyping(msg.channel);
 		})
 		return;
 	}
-	if (msg.author.id != bot.user.id) {
-		console.log("#" + msg.channel.name + ": " + msg.author.username + " - " + msg.content);
+	if (msg.author.id != bot.user.id && !(msg.channel.isPrivate)) {
+		console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content);
 	}
-	if (msg.channel.isPrivate && msg.author.id != bot.user.id) {
-		console.log("#private: " + msg.author.username + " - " + msg.content);
-		bot.sendMessage(msg.author, bot.user + " does not accept commands through private chat.");
-	}
+	if (msg.channel.isPrivate && msg.author.id != bot.user.id) {bot.sendMessage(msg.author, bot.user + " does not accept commands through private chat.");}
 	if ((msg.content === lastMessage && !(msg.channel.id === ("87901288729178112"))) && (msg.author.id === lastAuthor)) {
 		bot.deleteMessage(msg);
-		console.log("@WishBot - Deleted message from " + msg.author.name + " because it was a duplicate message.");
+		console.log(botC("@WishBot")+" - Deleted message from " + userC(msg.author.name) + " because it was a duplicate message.");
 	}
 	if (!(msg.channel.id == ("87901288729178112"))) {
 		lastAuthor = msg.author.id;
 		lastMessage = msg.content;
 	}
+	rand = Math.floor((Math.random() * 33) + 1); //Randomly Generates a number and then checks that number to see if it is 1 and if it is the game is randomly changed
+	if (rand == 1) {
+		var randgame = games[Math.floor(Math.random() * (games.length))];
+		bot.setPlayingGame(randgame);
+		console.log(botC("@WishBot")+" - Randomly changed game to \"" + warningC(randgame) + "\"");
+	}
 });
 
 //Login
 bot.login(options.email, options.password);
+console.log("Logged in using " + warningC(options.email));
 
 bot.on("disconnected", function () {
-console.log("Disconnected");
+console.log(errorC("Disconnected"));
 		setTimeout(function(){
-			console.log("info", "Attempting to log in...");
+			console.log(warningC("Attempting to re-connect..."));
 			bot.login(options.email, options.password, function (err, token) {
 			if (err) { console.log(err); process.exit(0); }
-			if (!token) { console.log("Failed to re-connect"); process.exit(0); }
-			});}, 20000);
+			if (!token) { console.log(errorC("Failed to re-connect")); process.exit(0); }
+		});}, 33333);
 });
-
-console.log("Logged in using " + options.email);
-//Server Functions
-function updateServers() {
-	fs.writeFile("./bot/servers.json", JSON.stringify(servers, null, '\t'), null);
-	servers = getServers();
-}
-
-function getServers() {
-	var svrs = require("./bot/servers.json");
-	return svrs;
-}
-
-function addServer(svr) {
-	if (svr.members.length < 101) {
-		var user_c = 1;
-		var s_g = 1;
-	} else {
-		var user_c = 0;
-		var s_g = 0;
-	}
-	var setngs = {
-		"username_change": user_c,
-		//"server_greeting": s_g,
-	}
-	servers[svr.id] = setngs;
-	updateServers();
-}
-
-function checkServers() {
-	bot.servers.forEach(function (ser) {
-		if (servers.hasOwnProperty(ser.id)) {} else {
-			addServer(ser);
-		}
-	});
-}
