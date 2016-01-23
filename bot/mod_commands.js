ab1c053aa72f6967c2f8dba8df50abe64c2afc4f
Evan Boudreau <xanizl@me.com>
2016-01-22 22:19:55 -0500

    Removed define command because wiki was better. Also added a delete thing for each command for future use

--- bot/mod_commands.js
+++ bot/mod_commands.js
@@ -17,13 +17,15 @@ function correctUsage(cmd) {
 }
 
 var mod_commands = {
-	"help": {
+	"help":
+	{
 		description: "Sends a DM containing all of these mod_commands.",
 		usage: "[command]",
+  	delete: false,
 		permLevel: 0,
 		process: function (bot, msg, suffix)
 		{
-			if(mod_commands[suffix])
+			if (mod_commands[suffix])
 			{
 				{
 					bot.sendMessage(msg.author, correctUsage(suffix))
@@ -35,9 +37,11 @@ var mod_commands = {
 				var msgArray = [];
 				msgArray.push("**mod_commands: **");
 				msgArray.push("```");
-				Object.keys(mod_commands).forEach(function (cmd) {
-					msgArray.push("" + options.mod_command_prefix + "" + cmd + ": " + mod_commands[cmd].description + "");
-				});
+				Object.keys(mod_commands)
+					.forEach(function (cmd)
+					{
+						msgArray.push("" + options.mod_command_prefix + "" + cmd + ": " + mod_commands[cmd].description + "");
+					});
 				msgArray.push("```");
 				bot.sendMessage(msg.author, msgArray);
 				bot.deleteMessage(msg);
@@ -48,132 +52,211 @@ var mod_commands = {
 	{
 		usage: "[none]",
 		description: "lists servers bot is connected to",
-		process: function (bot, msg) {
-			bot.sendMessage(msg.channel,bot.user+" is currently connected to the following servers:\n ```" +bot.servers+"```");
+  	delete: false,
+		process: function (bot, msg)
+		{
+			bot.sendMessage(msg.channel, bot.user + " is currently connected to the following servers:\n ```" + bot.servers + "```");
 			bot.deleteMessage(msg);
 		}
 	},
-	"stats": {
+	"stats":
+	{
 		usage: "[none]",
 		description: "outputs different about the bot",
-		process: function (bot, msg, suffix,commandsProcessed,talked) {
+  	delete: false,
+		process: function (bot, msg, suffix, commandsProcessed, talked)
+		{
 			var seconds = Math.round((bot.uptime / 1000) % 60);
 			var minutes = Math.round((bot.uptime / (1000 * 60)) % 60);
 			var hours = Math.round((bot.uptime / (1000 * 60 * 60)) % 60);
 			var statArray = []
-			statArray.push("**Info about " +bot.user+"**");
-			statArray.push("```Bot Uptime: "+hours+" hours, "+minutes+" minutes, and "+seconds+" seconds.");
-			statArray.push("Currently connected to "+bot.servers.length+" server(s) and "+bot.channels.length+" channel(s)");
-			statArray.push("During the current session "+commandsProcessed+" command(s) have been processed.");
-			statArray.push(bot.user.username+ "has been talked to "+talked+" time(s)");
-			statArray.push("Currently using "+ (Math.round(process.memoryUsage().rss/1024/1000))+ "MB of memory```")
+			statArray.push("**Info about " + bot.user + "**");
+			statArray.push("```Bot Uptime: " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds.");
+			statArray.push("Currently connected to " + bot.servers.length + " server(s) and " + bot.channels.length + " channel(s)");
+			statArray.push("During the current session " + commandsProcessed + " command(s) have been processed.");
+			statArray.push(bot.user.username + "has been talked to " + talked + " time(s)");
+			statArray.push("Currently using " + (Math.round(process.memoryUsage()
+				.rss / 1024 / 1000)) + "MB of memory```")
 			bot.sendMessage(msg, statArray);
 			bot.deleteMessage(msg);
 		}
 	},
-	"say": {
+	"say":
+	{
 		usage: "[message]",
 		description: "bot says message",
-		process: function (bot, msg, suffix) {
+		delete: false,
+		process: function (bot, msg, suffix)
+		{
 			bot.sendMessage(msg.channel, suffix);
 			bot.deleteMessage(msg);
 		}
 	},
-	"announce": {
+	"announce":
+	{
 		usage: "[message]",
 		description: "bot says message with text to speech",
-		process: function (bot, msg, suffix) {
-			bot.sendMessage(msg.channel, suffix, {
+    delete: false,
+		process: function (bot, msg, suffix)
+		{
+			bot.sendMessage(msg.channel, suffix,
+			{
 				tts: true
 			});
 			bot.deleteMessage(msg);
 		}
 	},
-	"join": {
+	"join":
+	{
 		usage: "[invite]",
 		description: "joins the server it's invited to",
-		process: function (bot, msg, suffix) {
-		bot.joinServer(suffix, function (error, server) {
-				if (error) {
+  	delete: false,
+		process: function (bot, msg, suffix)
+		{
+			bot.joinServer(suffix, function (error, server)
+			{
+				if (error)
+				{
 					bot.sendMessage(msg.channel, "Failed to join");
-					console.log(errorC("Failed to join - "+error));
+					console.log(errorC("Failed to join - " + error));
 				}
-				else {
+				else
+				{
 					console.log(warningC("Joined server " + server));
 					bot.sendMessage(msg.channel, "Successfully joined " + server);
 				}
 			});
 		}
 	},
-	"topic": {
+	"setcolour":
+	{
+		usage: "[mention] + [1-8 digit decimal colour code]",
+		description: "Creates a new role called \"Colour\" with a custom colour.",
+  	delete: false,
+		process: function (bot, msg, suffix)
+		{
+			if (suffix && msg.mentions && suffix.split(" ")[1] && suffix.split(" ")[1].length < [9] && /^\d+$/.test(suffix.split(" ")[1]))
+			{
+				var colour = (suffix.split(" ")[1]).toString();
+				msg.mentions.map(function (usr)
+					{
+						bot.createRole(msg.channel.server,
+							{
+								position: [2],
+								name: "Colour " + colour,
+								color: colour,
+							})
+							.then(function (permission)
+							{
+								bot.addMemberToRole(usr.id, permission)
+									.then(function ()
+									{
+										bot.sendMessage(msg.channel, "Applied the colour "+colour+" to <@" + usr.id + ">.");
+										console.log(channelC("#" + msg.channel.name) + ": " + botC("@WishBot") + " - New Role was made " + warningC("Colour ") + colour + " by " + userC(msg.author.username));
+									})
+							})
+					})
+			}
+			else
+			{
+				bot.sendMessage(msg.author, correctUsage("setcolour"));
+				bot.sendMessage(msg.author, "Use http://www.mathsisfun.com/hexadecimal-decimal-colors.html to get a decimal colour code.");
+			}
+			bot.deleteMessage(msg);
+		}
+	},
+	"topic":
+	{
 		usage: "[topic]",
 		description: "Sets the topic for the channel. No topic removes the topic.",
-		process: function (bot, msg, suffix) {
+  	delete: false,
+		process: function (bot, msg, suffix)
+		{
 			bot.setChannelTopic(msg.channel, suffix);
-			console.log(botC("@WishBot - ")+warningC("Set topic of "+msg.channel));
+			console.log(botC("@WishBot - ") + warningC("Set topic of " + msg.channel));
 			bot.reply(msg, "done!")
 		}
 	},
-	"playing": {
+	"playing":
+	{
 		usage: "[game]",
 		description: "allows you to set a game for Onee-chan to play. If nothing specified it will be random.",
-		process: function (bot, msg, suffix) {
-			if (suffix) {
+  	delete: false,
+		process: function (bot, msg, suffix)
+		{
+			if (suffix)
+			{
 				bot.setPlayingGame(suffix);
 			}
-			if (!suffix) {
+			if (!suffix)
+			{
 				bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))]);
 			}
 			bot.deleteMessage(msg);
 		}
 	},
-	"stop": {
+	"stop":
+	{
 		usage: "[none]",
 		description: "stops the bot",
-		process: function (bot, msg, suffix) {
-			if (msg.author.id === "87600987040120832") {
+  	delete: false,
+		process: function (bot, msg, suffix)
+		{
+			if (msg.author.id === "87600987040120832")
+			{
 				bot.reply(msg, "I will be taking my leave.")
 				bot.logout();
-				setTimeout(function(){
+				setTimeout(function ()
+				{
 					console.log("@WishBot - Stopped bot.");
 					process.exit(0);
 				}, 1000);
 			}
-			else {
+			else
+			{
 				bot.reply(msg, "I CANNOT BE STOPPED BY THE LIKES OF YOU");
 			}
 			bot.deleteMessage(msg);
 		}
 	},
-	"setname": {
+	"setname":
+	{
 		usage: "[name]",
 		description: "Changes the bots username",
-		process: function (bot, msg, suffix) {
-			if (suffix) {
+		delete: false,
+		process: function (bot, msg, suffix)
+		{
+			if (suffix)
+			{
 				bot.setUsername(suffix);
-				console.log(channelC("#" + msg.channel.name) + ": "+botC("@WishBot")+" - Username set to "+warningC(suffix)+" by " + userC(msg.author.username));
-			} else {
+				console.log(channelC("#" + msg.channel.name) + ": " + botC("@WishBot") + " - Username set to " + warningC(suffix) + " by " + userC(msg.author.username));
+			}
+			else
+			{
 				bot.setUsername("Onee-chan");
-				console.log(channelC("#" + msg.channel.name) + ": "+botC("@WishBot")+" - Username set to "+warningC("Onee-chan")+" by " + userC(msg.author.username));
+				console.log(channelC("#" + msg.channel.name) + ": " + botC("@WishBot") + " - Username set to " + warningC("Onee-chan") + " by " + userC(msg.author.username));
 			}
 			bot.reply(msg, "done!")
 		}
 	},
-	"delete": {
+	"delete":
+	{
 		usage: "[number of messages from 1 - 100]",
 		description: "Deletes the specified number of messages from the channel",
+  	delete: false,
 		process: function (bot, msg, suffix)
 		{
-			if(suffix && /^\d+$/.test(suffix))
+			if (suffix && /^\d+$/.test(suffix))
 			{
-				bot.getChannelLogs(msg.channel, 100, function(error,messages)
+				bot.getChannelLogs(msg.channel, 100, function (error, messages)
 				{
-					if(error)
+					if (error)
 					{
 						console.log("there was an error getting the logs");
 						return;
 					}
-					else {
+					else
+					{
 						bot.startTyping(msg.channel);
 						var deletes = parseInt(suffix, 10) + 1;
 						var dones = 0;
@@ -182,7 +265,7 @@ var mod_commands = {
 							bot.deleteMessage(count);
 							dones++;
 							deletes--;
-							if(deletes == 0 || dones == 100)
+							if (deletes == 0 || dones == 100)
 							{
 								bot.stopTyping(msg.channel);
 								return;
@@ -194,94 +277,21 @@ var mod_commands = {
 			}
 			else
 			{
-					bot.reply(msg,"using the delete command requires a interger between 1-500")
+				bot.reply(msg, "using the delete command requires a interger between 1-500")
 			}
 		}
-	}/*,
-	"setcolour": {
-		usage: "[name]",
-		description: "Changes the bots username",
-		process: function (bot, msg, suffix) {
-			if (suffix && msg.mentions)
-			{
-				var colour = (suffix.split(" ")[1]);
-				var temp = colour.toString();
-				msg.mentions.map(function (usr)
-				{
-					bot.createRole(msg.channel.server.id,
-					{
-								color : temp,
-    						name : temp
-					})
-					setTimeout(function()
-					{
-						bot.addMemberToRole(usr,temp);
-					},1000);
-				});
-
-			}
-			else
-			{
-				bot.sendMessage(msg.channel, "You need to enter a colour before I can change you to that Senpai.")
-			}
-		}
-	},
-	/*,
-	"update": {
-		usage: "[none]",
-		description:"Updates the bot to the most current veresion on github",
-		process: function(bot, msg)
-		{
-			if (msg.author.id === "87600987040120832" && options.github)
-			{
-				bot.sendMessage(msg.channel,"fetching updates...",function(error,sentMsg){
-						console.log("updating...");
-					var spawn = require('child_process').spawn;
-						var log = function(err,stdout,stderr){
-								if(stdout){console.log(stdout);}
-								if(stderr){console.log(stderr);}
-						};
-						var fetch = spawn('git', ['fetch']);
-						fetch.stdout.on('data',function(data){
-								console.log(data.toString());
-						});
-						fetch.on("close",function(code){
-								var reset = spawn('git', ['reset','--hard','origin/master']);
-								reset.stdout.on('data',function(data){
-										console.log(data.toString());
-								});
-								reset.on("close",function(code){
-										var npm = spawn('npm', ['install']);
-										npm.stdout.on('data',function(data){
-												console.log(data.toString());
-										});
-										npm.on("close",function(code){
-												console.log("goodbye");
-												bot.sendMessage(msg.channel,"brb!",function(){
-														bot.logout(function(){
-																process.exit();
-														});
-												});
-										});
-								});
-						});
-				});
-			}
-			else
-			{
-
-			}
-		}
-	}*/
+	}
 	/*,
 	"eval": {
 		usage: "[command]",
 		description: 'Executes arbitrary javascript in the bot process.',
+		delete: false,
 		process: function(bot,msg,suffix) {
 				bot.sendMessage(msg.channel, eval(suffix,bot));
 		}
 	}*/
 };
+
 
 function rssfeed(bot, msg, url, count, full) {
 	var FeedParser = require('feedparser');
