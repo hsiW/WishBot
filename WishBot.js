29deb6c4af8ceb57f6b325727163cca9873030cd
Evan Boudreau <xanizl@me.com>
2016-01-24 15:33:13 -0500

    Re-wrote alot of the check message code

--- WishBot.js
+++ WishBot.js
@@ -13,7 +13,6 @@ cleverbot.prepare(function () {});
 
 var chalk = require("chalk");
 var c = new chalk.constructor({enabled: true});
-
 var serverC = c.black.bold;
 var channelC = c.green.bold;
 var userC = c.cyan.bold;
@@ -21,11 +20,9 @@ var warningC = c.yellow.bold;
 var errorC = c.red.bold;
 var botC = c.magenta.bold;
 
-var lastMessage;//used to store the last message for use in spam detection
-var lastAuthor;//used to store the last author for use in spam detection
+var lastMessage = new Object();//used to store the last message for use in spam detection
 var commandsProcessed = 0;//used to count the ammount of commands processed in the current session
 var talked = 0;//used to count how many times people talked to WishBot in the current session
-var rand = 0;//used for randomly pikcing game later on in code
 
 //Does this stuff when the bot is ready and running
 bot.on("ready", function ()
@@ -36,8 +33,21 @@ bot.on("ready", function ()
 
 //Does this stuff when the bot detects a message, can be in a channel its part of or through a private chat
 bot.on("message", function (msg) {
-	//Start of command checking
+	if (msg.content === lastMessage.text && msg.author.id === lastMessage.lastAuthor  && msg.channel.id === lastMessage.channel)
+	{bot.deleteMessage(msg).then(console.log(serverC(botC("@WishBot")+" - Deleted message from " + userC(msg.author.name) + " because it was a duplicate message.")))}
+	if(msg.author.id == bot.user.id){return;}
 	var suffix = msg.content.substring((msg.content.split(" ")[0].substring(1)).length + 2);
+	if (!(msg.channel.isPrivate) && (msg.content.indexOf(bot.user.mention()) == 0)) {
+		console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content);
+		bot.startTyping(msg.channel);
+		talked = talked + 1;
+		onee.write(suffix, function (response) {
+			console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": "+botC("@WishBot")+" - 🌐 - " + response.message);
+			bot.sendMessage(msg.channel, "🌐 - " + response.message).then(bot.stopTyping(msg.channel));
+		})
+		return;
+	}
+	if (msg.channel.isPrivate && msg.author.id != bot.user.id) {bot.sendMessage(msg.author, bot.user + " does not accept commands through private chat.")}
 	if (!(msg.channel.isPrivate) && ((msg.content[0] === options.command_prefix) || (msg.content[0] === options.mod_command_prefix)) && (msg.author.id != bot.user.id)) {
 		var cmdTxt = msg.content.split(" ")[0].substring(1);
 		if ((commands[cmdTxt] && msg.content[0] === options.command_prefix) || (mod_commands[cmdTxt] && msg.content[0] === options.mod_command_prefix && msg.channel.permissionsOf(msg.sender).hasPermission("manageRoles")))
@@ -46,41 +56,13 @@ bot.on("message", function (msg) {
 			if (commands[cmdTxt] && msg.content[0] === options.command_prefix){var cmd = commands[cmdTxt]}
 			if (mod_commands[cmdTxt] && msg.content[0] === options.mod_command_prefix){var cmd = mod_commands[cmdTxt]}
 			console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": "+botC("@WishBot")+" - "+warningC(cmdTxt) + " was used by " + userC(msg.author.username));
-			bot.sendMessage("@"+msg.channel.server.name+": #" + msg.channel.name + ": @WishBot - `" + cmdTxt + "` was used by " + msg.author.username);
-			cmd.process(bot, msg, suffix, commandsProcessed, talked);
+			bot.sendMessage("@"+msg.channel.server.name+": #" + msg.channel.name + ": @WishBot - `" + cmdTxt + "` was used by " + msg.author.username).then(cmd.process(bot, msg, suffix, commandsProcessed, talked));
 			if(cmd.delete){bot.deleteMessage(msg)}
 		}
 	}
-	if (!(msg.channel.isPrivate) && (msg.content.indexOf(bot.user.mention()) == 0) && (msg.author.id != bot.user.id)) {
-		console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content);
-		var conv = suffix.split(" ");
-		bot.startTyping(msg.channel);
-		talked = talked + 1;
-		onee.write(conv, function (response) {
-			console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": "+botC("@WishBot")+" - 🌐 - " + response.message);
-			bot.sendMessage(msg.channel, "🌐 - " + response.message);
-			bot.stopTyping(msg.channel);
-		})
-		return;
-	}
-	if (msg.author.id != bot.user.id && !(msg.channel.isPrivate)) {
-		console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content);
-	}
-	if (msg.channel.isPrivate && msg.author.id != bot.user.id) {bot.sendMessage(msg.author, bot.user + " does not accept commands through private chat.");}
-	if ((msg.content === lastMessage && !(msg.channel.id === ("87901288729178112"))) && (msg.author.id === lastAuthor)) {
-		bot.deleteMessage(msg);
-		console.log(botC("@WishBot")+" - Deleted message from " + userC(msg.author.name) + " because it was a duplicate message.");
-	}
-	if (!(msg.channel.id == ("87901288729178112"))) {
-		lastAuthor = msg.author.id;
-		lastMessage = msg.content;
-	}
-	rand = Math.floor((Math.random() * 33) + 1); //Randomly Generates a number and then checks that number to see if it is 1 and if it is the game is randomly changed
-	if (rand == 1) {
-		var randgame = games[Math.floor(Math.random() * (games.length))];
-		bot.setPlayingGame(randgame);
-		console.log(botC("@WishBot")+" - Randomly changed game to \"" + warningC(randgame) + "\"");
-	}
+	if (msg.channel.id != ("87901288729178112")) {lastMessage = {text:msg.content, lastAuthor:msg.author.id, channel:msg.channel.id}}
+	if (Math.floor((Math.random() * 33) + 1) == 1) {bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))])}
+	else if (msg.author.id != bot.user.id && !(msg.channel.isPrivate)) {console.log(serverC("@"+msg.channel.server.name+":")+channelC(" #" + msg.channel.name) + ": " + userC(msg.author.username) + " - " + msg.content)}
 });
 
 //Login
