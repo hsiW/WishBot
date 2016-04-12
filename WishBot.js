bb185e0ad2f684260185e8187c09f70154a99167
Evan Boudreau <xanizl@me.com>
2016-04-11 21:56:48 -0400

    2.0 => 2.1.5 Changes
    Recoded database stuff so it causes less errors.
    Added ignore database to ban bot abusers.
    Added cooldowns to help with ratelimiting.
    Added Smug, 8ball, sneakylenny, uptime and many more commands.
    Recoded a large portion of things.
    Lets just say I did alot

--- WishBot.js
+++ WishBot.js
@@ -1,45 +1,50 @@
-var Discord = require("discord.js"), bot = new Discord.Client({forceFetchUsers: true});
+var Discord = require("discord.js"), bot = new Discord.Client({forceFetchUsers: true, maxCachedMessages: 5});
 var request = require('request');
 var options = require("./options/options.json"), prefix = require("./options/options.json").prefixes;
 var games = require("./lists/games.json").games;
-var defaults = require("./bot_modules/Defaults.js").defaults;
 var CmdHandler = require("./CommandHandler.js").Commands;
 var db = require("./bot_modules/Database.js");
 var chalk = require("chalk"), c = new chalk.constructor({enabled: true});
 var serverC = c.black.bold, channelC = c.green.bold, userC = c.cyan.bold, warningC = c.yellow.bold, errorC = c.red.bold, botC = c.magenta.bold;
 var tablesUnFlipped = ["┬─┬﻿ ︵ /(.□. \\\\)", "┬─┬ノ( º _ ºノ)", "┬─┬﻿ ノ( ゜-゜ノ)", "┬─┬ ノ( ^_^ノ)", "┬──┬﻿ ¯\\\\_(ツ)", "(╯°□°）╯︵ /(.□. \\\\)"]
 var welcome = [];
+var admins = require("./options/admins.json").admins;
 var pg = require('pg');
 
 pg.on('error', function() {console.log("Error with Database");})
 
-bot.on("ready", function (){bot.setPlayingGame("Use -help for info");console.log(botC("@WishBot")+" - Ready! | Channels: " + channelC(bot.channels.length) + " | Servers: "+serverC(bot.servers.length)+" | Users: "+userC(bot.users.length)); serverPost();})
+bot.on("ready", function (){
+	bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))]);
+	console.log(botC("@WishBot")+" - Ready!");
+})
 
-bot.on("message", function (msg) {
+bot.on("message", msg => {
 	if(msg.author === bot.user || msg.channel.isPrivate) return;
 	else if((/[a-z]/i.test(msg.content[0]) == false) && msg.channel.server){
-	db.Settings(msg, function(loadedEntity) {
-	if((msg.content == "(╯°□°）╯︵ ┻━┻" || msg.content === "/tableflip") && msg.author.id != bot.user.id){bot.sendMessage(msg, tablesUnFlipped[Math.floor(Math.random() * (tablesUnFlipped.length))])}
-	var suffix = msg.content.substring((msg.content.split(" ")[0].substring(1)).length + 2);
-	if(msg.content.indexOf(bot.user.mention()) == 0){CmdHandler(bot, msg, suffix, loadedEntity); return;}
-	else if (msg.content[0] === prefix[1]) {CmdHandler(bot, msg, suffix); return;}
-	else if (loadedEntity && msg.content[0] === loadedEntity.Prefix){CmdHandler(bot, msg, suffix, loadedEntity); return;}
-	else if (!loadedEntity && prefix.indexOf(msg.content[0]) == 0) {CmdHandler(bot, msg, suffix); return;}
-	else if (msg.content.split(" ")[0] === "1nfo"){console.log(loadedEntity);}
-	else if (msg.content.split(" ")[0] === "$$eval$$" && msg.author.id === "87600987040120832"){things(msg, suffix);}
-});
-}
+		if((msg.content == "(╯°□°）╯︵ ┻━┻") && msg.author.id != bot.user.id){bot.sendMessage(msg, tablesUnFlipped[Math.floor(Math.random() * (tablesUnFlipped.length))]); return;}
+		db.Settings(msg, function(loadedEntity) {
+			var suffix = msg.content.substring((msg.content.split(" ")[0].substring(1)).length + 2);
+			if(msg.content.indexOf(bot.user.mention()) == 0) CmdHandler(bot, msg, suffix, loadedEntity);
+			else if (msg.content.split(" ")[0] === "-=eval" && msg.author.id === "87600987040120832") things(msg, suffix);
+			else if (msg.content === "-prefix") CmdHandler(bot, msg, suffix);
+			else if (msg.content[0] === prefix[1] && (admins.indexOf(msg.author.id) > -1)) CmdHandler(bot, msg, suffix);
+			else if (loadedEntity && msg.content[0] === loadedEntity.Prefix) CmdHandler(bot, msg, suffix, loadedEntity);
+			else if (!loadedEntity && prefix.indexOf(msg.content[0]) == 0) CmdHandler(bot, msg, suffix);
+		});
+	}
 });
 
-bot.on("serverCreated", function (server){
-	welcome.push("Hello!\nI'm **WishBot**, better known as "+bot.user+".\nI was written by **Mᴉsɥ** using *Discord.js.*")
-	welcome.push("My \"server\" can be found by using `-server`.\nFor information on what I can do use `-help`")
-	welcome.push("Thanks!")
-	bot.sendMessage(server.defaultChannel, welcome)
-	console.log(serverC("@"+server.name+":")+channelC(" #" + server.defaultChannel.name) + ": " + userC("@WishBot") + " - Joined Server!")
-	console.log(botC("@WishBot")+" - Channels: " + channelC(bot.channels.length) + " | Servers: "+serverC(bot.servers.length));
-	welcome = [];
-});
+bot.on("serverDeleted", server =>{
+	console.log(serverC("@"+server.name+": ")+botC("@WishBot")+" - "+errorC("Left Server"));
+})
+
+bot.on("serverCreated", server =>{
+	console.log(serverC("@"+server.name+": ")+botC("@WishBot")+" - "+errorC("Joined Server"));
+})
+
+bot.on("error", err =>{
+	console.log(botC("@WishBot")+" - "+errorC("ERROR: "+err));
+})
 
 function serverPost(){
 	request.post({
@@ -47,7 +52,7 @@ function serverPost(){
 		"headers": {"content-type": "application/json"},
 		"json": true,
 		body: {
-			"key": process.env.carbon_key,
+			"key": options.carbon_key,
 			"servercount": bot.servers.length
 		}
 	},(error, response, body)=>{
@@ -63,10 +68,9 @@ function things(msg, suffix) {
 	if (result && typeof result !== "object") {bot.sendMessage(msg, result);}
 }
 
-if(!options.private){bot.loginWithToken(options.token);console.log(warningC("Logged in using "+botC("Token")));}
-else{bot.loginWithToken(process.env.token);console.log(warningC("Logged in using "+botC("Token")));}
+bot.loginWithToken(options.token);console.log(warningC("Logged in using "+botC("Token")));
 
-setInterval(()=>{serverPost()}, 3600000);
-
-
-setInterval(() => {bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))])}, 333333);
+if(options.carbon_key !== ""){
+	setInterval(()=> serverPost(), 3600000);
+}
+setInterval(() => bot.setPlayingGame(games[Math.floor(Math.random() * (games.length))]), 333333);
