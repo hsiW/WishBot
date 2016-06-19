c1706d1d67cf82225179aeb4a78ca6c2cc527f11
hsiw <xanizl@me.com>
2016-06-19 18:09:13 -0400

    Changed anilist to airing and added air times to the currently running animes.

--- commands/anime/airing.js
+++ commands/anime/airing.js
@@ -1,10 +1,13 @@
-var nani = require('nani').init('hsiw-gt2fs', 't9C9EyoLpMsqvNRldKB9pK');
+var options = require('./../../options/options.json');
+var nani = require('nani').init(options.nani_id, options.nani_secret);
 var getSeason = require('./../../utils/utils.js').getSeason;
 var toTitleCase = require('./../../utils/utils.js').toTitleCase;
 var splitArray = require('./../../utils/utils.js').splitArray;
 
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 var seasons = ['winter', 'spring', 'summer', 'fall'];
-var url = `browse/anime?season=${seasons[getSeason()]}&full_page=true&type=Tv&airing_data=true`
+var url = `browse/anime?season=${seasons[getSeason()]}&full_page=true&type=Tv&airing_data=true&sort=popularity-desc`
 var currentAnime = null;
 
 module.exports = {
@@ -15,8 +18,8 @@ module.exports = {
         if (currentAnime === null) {
             nani.get(url)
                 .then(data => {
-                	console.log(data)
-                    currentAnime = Object.keys(data).map(title => data[title].title_romaji).sort();
+                    console.log(data)
+                    currentAnime = data.filter(isAiring).map(title => title.title_romaji + " | " + weekday[new Date(title.airing.time).getDay()]);
                     sendAnime(bot, msg, suffix)
                 })
                 .catch(error => {
@@ -30,9 +33,13 @@ module.exports = {
 
 function sendAnime(bot, msg, suffix) {
     var pagedTitles = splitArray(currentAnime, 10);
-    (/^[0-9]$/.test(suffix) && suffix < pagedTitles.length) ? page = suffix - 1 : page = 0;
-    var msgString = `\`\`\`markdown\n### Anime for the ${toTitleCase(seasons[getSeason()])} Season: (Page #${page+1}) ###\n`;
+    (/^[0-9]$/.test(suffix) && (suffix - 1 <= pagedTitles.length)) ? page = suffix - 1 : page = 0;
+    console.log(page);
+    var msgString = `\`\`\`markdown\n### Anime for the ${toTitleCase(seasons[getSeason()])} Season: (Page ${page+1}/${pagedTitles.length}) ###\n`;
     msgString += pagedTitles[page].map((value, position) => '[' + (position + 1 + (page * 10)) + ']: ' + value).join('\n');
     bot.createMessage(msg.channel.id, msgString + "```").catch(console.log);
+}
 
+function isAiring(title) {
+    if (title.airing != null) return title;
 }
