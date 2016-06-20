c29e8bc4c5631f846e6201cc77cfd77f50e35a26
hsiw <xanizl@me.com>
2016-06-20 17:26:58 -0400

    Made airing not limited to the current season and added episode count

--- commands/anime/airing.js
+++ commands/anime/airing.js
@@ -7,9 +7,8 @@ var secondsToString = require('./../../utils/utils.js').secondsToString;
 
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var seasons = ['winter', 'spring', 'summer', 'fall'];
-var url = `browse/anime?season=${seasons[getSeason()]}&full_page=true&type=Tv&airing_data=true&sort=popularity-desc`
+var url = `browse/anime?status=Currently%20Airing&full_page=true&type=Tv&airing_data=true&sort=popularity-desc`
 var airingAnime;
-var currentAnime;
 
 module.exports = {
     usage: "This bot dances around in the current channel using a random dance",
@@ -19,7 +18,7 @@ module.exports = {
         nani.get(url)
             .then(data => {
                 airingAnime = data.filter(isAiring);
-                currentAnime = data.filter(isAiring).map(title => title.title_english + " | " + weekday[new Date(title.airing.time).getDay()]);
+
                 processAnime(bot, msg, suffix);
             })
             .catch(error => {
@@ -29,15 +28,14 @@ module.exports = {
 }
 
 function processAnime(bot, msg, suffix) {
-    if (/^[0-9]$/.test(suffix)) sendAnime(bot, msg, suffix);
+    if (/^[0-9]$/.test(suffix) || !suffix) sendAnime(bot, msg, suffix);
     else if (airingAnime.find(title => isAnime(title, suffix))) {
         anime = airingAnime.find(title => isAnime(title, suffix));
         var msgString = '```ruby\n';
         msgString += `Title: ${anime.title_english}\n`
         msgString += `Romaji Title: ${anime.title_romaji}\n`;
-        msgString += `Japanese Title: ${anime.title_japanese}\n`;
-        msgString += `Average Score: ${anime.average_score}/100\n\n`
-        msgString += `Next Airing: ${(new Date(anime.airing.time)).toUTCString()}\n`;
+        msgString += `Japanese Title: ${anime.title_japanese}\n\n`;
+        msgString += `Next Episode: ${anime.airing.next_episode}/${anime.total_episodes} | Airing: ${(new Date(anime.airing.time)).toUTCString()}\n`;
         msgString += `Time Til Airing ${secondsToString(anime.airing.countdown)}`
         bot.createMessage(msg.channel.id, msgString + "```")
     } else bot.createMessage(msg.channel.id, 'No Airing Anime found called `' + suffix + '`, **' + msg.author.username + '**-senpai.');
@@ -45,8 +43,9 @@ function processAnime(bot, msg, suffix) {
 
 
 function sendAnime(bot, msg, suffix) {
+    var currentAnime = airingAnime.map(title => title.title_english + " | " + weekday[new Date(title.airing.time).getDay()]);
     var pagedTitles = splitArray(currentAnime, 10);
-    (suffix - 1 <= pagedTitles.length) ? page = suffix - 1 : page = 0;
+    ((suffix - 1 <= pagedTitles.length) && suffix) ? page = suffix - 1 : page = 0;
     var msgString = `\`\`\`markdown\n### Anime for the ${toTitleCase(seasons[getSeason()])} Season: (Page ${page+1}/${pagedTitles.length}) ###\n`;
     msgString += pagedTitles[page].map((value, position) => '[' + (position + 1 + (page * 10)) + ']: ' + value).join('\n');
     bot.createMessage(msg.channel.id, msgString + "```").catch(console.log);
