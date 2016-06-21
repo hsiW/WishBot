36ecc9dcae3f0e99ead550ddcfe5a2b975ee830d
hsiw <xanizl@me.com>
2016-06-21 08:52:35 -0400

    Delete update.bat

--- update.bat
+++
@@ -1,4 +0,0 @@
-RD /S /Q "C:\Users\Evan\Documents\WishBot\WishBot_Rewrite\Eris"
-git clone https://github.com/abalabahaha/Eris.git
-npm install C:\Users\Evan\Documents\WishBot\WishBot_Re-Write\Eris
-\k
