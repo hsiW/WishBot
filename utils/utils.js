5ab279dc83575fc8ec18ac21247e697b10632a1e
hsiw <xanizl@me.com>
2016-08-08 11:31:21 -0400

    Function Cleanup

--- utils/utils.js
+++ utils/utils.js
@@ -1,16 +1,16 @@
-exports.toTitleCase = function(str) {
+exports.toTitleCase = str => {
     return str.replace(/\w\S*/g, function(txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
 
-exports.getName = function(msg, name) {
+exports.getName = (msg, name) => {
     let nameRegex = new RegExp(name, "i");
     if (!name) return msg.channel.guild.members.get(msg.author.id).user;
     else return msg.channel.guild.members.find(member => (member.nick || member.user.username).match(nameRegex)) ? msg.channel.guild.members.find(member => (member.nick || member.user.username).match(nameRegex)).user : msg.channel.guild.members.get(msg.author.id).user;
 }
 
-exports.sortObj = function(obj, type, caseSensitive) {
+exports.sortObj = (obj, type, caseSensitive) => {
     let temp_array = [];
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
@@ -42,7 +42,7 @@ exports.sortObj = function(obj, type, caseSensitive) {
     return temp_obj;
 };
 
-exports.minutesToString = function(mins) {
+exports.minutesToString = mins => {
     let months = Math.floor(mins / 43800);
     let days = Math.floor((mins / 1440) % 30);
     let hours = Math.floor((mins / 60) % 24);
@@ -50,7 +50,7 @@ exports.minutesToString = function(mins) {
     return months + "month(s) | " + days + "day(s) | " + hours + "hour(s) | " + minutes + "minute(s)";
 }
 
-exports.daysToString = function(day) {
+exports.daysToString = day => {
     let months = Math.floor(day / 30)
     let days = Math.floor(day % 30)
     let hours = Math.floor((day % 24) % 30)
@@ -58,7 +58,7 @@ exports.daysToString = function(day) {
     return months + "month(s) | " + days + "day(s) | " + hours + "hour(s) | " + minutes + "minute(s)";
 }
 
-exports.secondsToString = function(second) {
+exports.secondsToString = second => {
     let days = Math.floor(second / 86400)
     let hours = Math.floor((second / 3600) % 24)
     let minutes = Math.floor((second / 60) % 60)
@@ -66,7 +66,7 @@ exports.secondsToString = function(second) {
     return days + "day(s) | " + hours + "hour(s) | " + minutes + "minute(s) | " + seconds + "second(s)";
 }
 
-exports.splitArray = function(array, size) {
+exports.splitArray = (array, size) => {
     let sets = [],
         chunks,
         i = 0;
