95dd98957113fcf39c1b957a5850a328e069aaea
hsiw <xanizl@me.com>
2016-10-15 13:47:19 -0400

    Reformated info command

--- commands/bot/info.js
+++ commands/bot/info.js
@@ -1,25 +1,27 @@
-let libVersion = require('./../../node_modules/eris/package.json').version, 
-    botVersion = require('./../../package.json').version, 
-    prefix = require('./../../options/options.json').prefix; 
+let libVersion = require('./../../node_modules/eris/package.json').version,
+    botVersion = require('./../../package.json').version,
+    prefix = require('./../../options/options.json').prefix;
+
+module.exports = {
+    usage: "Returns info about the bot, including a link to the support server as well as a link to the bots source code.",
+    delete: false,
+    cooldown: 10,
+    process: (msg, args, bot) => {
+        return Promise.resolve({
+            message: `\`\`\`markdown
+[WishBot Info](${bot.user.username}) 
+
+# About this Bot
+[Main Developer](Mei#5429) 
+[Default Prefix](${prefix})
+[Bot Version](v${botVersion})
+[Discord Library](Eris - v${libVersion})  
  
-module.exports = { 
-    usage: "Returns basic info about this bot.", 
-    cooldown: 30, 
-    process: (msg, args, bot) => { 
-        return Promise.resolve({ 
-            message: `
-\`\`\`tex 
-$ WishBot \\${bot.user.username}\\ $ 
- 
-Lib: { Eris - v${libVersion} } 
-Version: { v${botVersion} } 
-Creator: { Mei#5429 } 
-Default Prefix: { ${prefix} } 
- 
-% Use ${prefix}help for command info. 
-% Support Server: https://discord.gg/0lBiROCNVaGw5Eqk 
-% Source: https://github.com/hsiw/Wishbot\`\`\`
-` 
-        }) 
-    } 
-}//will update to new format later today
+# Use ${prefix}help a list of the current bot commands.
+[Source](https://github.com/hsiw/Wishbot)
+[Support Server](https://discord.gg/0lBiROCNVaGw5Eqk)
+\`\`\`
+`
+        })
+    }
+}
