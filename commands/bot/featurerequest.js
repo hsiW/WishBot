9c637091cf236e7fb75c1fa56c2fbe8ac7404564
hsiw <xanizl@me.com>
2016-10-15 09:52:52 -0400

    Updated featurerequest
    Uses new command format as well as having new overall layout

--- commands/bot/featurerequest.js
+++ commands/bot/featurerequest.js
@@ -1,10 +1,23 @@
 module.exports = {
     usage: 'Sends a feature request to the maker of the bot.\n`request [feature to request]`',
-    delete: true,
+    dm: false,
     cooldown: 60,
-    process: (bot, msg, suffix) => {
-        if (!suffix) return;
-        bot.createMessage(msg.channel.id, 'Your request for \'**' + suffix + '**\' was successfully sent, **' + msg.author.username + '**-senpai.').catch()
-        bot.createMessage('142794318837579777', '__Requested on the server **' + msg.channel.guild.name + '** by **' + msg.author.username + '**:__`' + msg.author.id + '`\n' + suffix).catch();
+    process: (msg, args, bot) => {
+        return new Promise(resolve => {
+            if (!args) resolve({
+                message: `You must enter something to be requested, **${msg.author.username}**-senpai`,
+                delete: true
+            })
+            else if (args.length > 1750) resolve({
+                message: `Requests cannot be over 1750 characters, sorry **${msg.author.username}**-senpai`,
+                delete: true
+            })
+            else {
+                bot.createMessage('142794318837579777', `**${msg.channel.guild.name}**(*${msg.channel.guild.id}*) - **${msg.author.username}**(*${msg.author.id}*)
+\`\`\`${args}\`\`\``).then(() => resolve({
+                    message: `Your request for **'${args}'** was successfully sent, **${msg.author.username}**-senpai.`
+                }))
+            }
+        });
     }
 }
