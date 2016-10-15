46ab3d1532b670671ca0d77eefb323ab959a7b22
hsiw <xanizl@me.com>
2016-10-15 15:31:13 -0400

    Added comments to ping

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -4,7 +4,8 @@ module.exports = {
     cooldown: 5,
     process: msg => {
         return Promise.resolve({
-            message: "PONG!",
+            message: "PONG!", //Sends Pong! on message
+            //Uses a function to configure the time between the message recieving th message and the new sent one so the time will be more accurate
             edit: message => 'PONG! | *' + (new Date(message.timestamp) - new Date(msg.timestamp)) + '*ms'
         });
     }
