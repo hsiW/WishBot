e4c5d2787d3c565ad61dfb39ff2d9af123d17985
hsiw <xanizl@me.com>
2016-10-15 15:44:16 -0400

    Commented featurerequest

--- commands/bot/featurerequest.js
+++ commands/bot/featurerequest.js
@@ -1,20 +1,24 @@
 module.exports = {
-    usage: 'Sends a feature request to the maker of the bot.\n`request [feature to request]`',
+    usage: 'Sends a feature request to the maker of the bot. Requests can have a max length of 1750 characters\n`request [feature to request]`',
     dm: false,
     cooldown: 60,
     process: (msg, args, bot) => {
         return new Promise(resolve => {
+            //If nothing is requested yell at the user for not doing so
             if (!args) resolve({
-                message: `You must enter something to be requested, **${msg.author.username}**-senpai`,
-                delete: true
-            })
+                    message: `You must enter something to be requested, **${msg.author.username}**-senpai`,
+                    delete: true
+                })
+                //If the request is potentially to large tell the user so
             else if (args.length > 1750) resolve({
                 message: `Requests cannot be over 1750 characters, sorry **${msg.author.username}**-senpai`,
                 delete: true
             })
             else {
+                //Create a message in the featureRequest channel in the Yuki-chan server with the users request
                 bot.createMessage('142794318837579777', `**${msg.channel.guild.name}**(*${msg.channel.guild.id}*) - **${msg.author.username}**(*${msg.author.id}*)
 \`\`\`${args}\`\`\``).then(() => resolve({
+                    //Tell the user that their request was successfully sent to the request channel on promise resolve
                     message: `Your request for **'${args}'** was successfully sent, **${msg.author.username}**-senpai.`
                 }))
             }
