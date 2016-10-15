438eb68d4e508f2436b1c51746f3ca0b7234b1cc
hsiw <xanizl@me.com>
2016-10-15 14:55:41 -0400

    Updated clean a bit

--- commands/bot/clean.js
+++ commands/bot/clean.js
@@ -5,8 +5,8 @@ module.exports = {
     cooldown: 10,
     process: (msg, args, bot) => {
         return new Promise(resolve => {
-            /^\d+$/.test(args) ? args : args = 50;
-            if (msg.channel.permissionsOf(bot.user.id).has('manageMessages')) bot.purgeChannel(msg.channel.id, parseInt(args), message => message.author.id === bot.user.id)
+            /^\d+$/.test(args) ? args = parseInt(args) : args = 50;
+            if (msg.channel.permissionsOf(bot.user.id).has('manageMessages')) msg.channel.purge(args, message => message.author.id === bot.user.id)
                 .then(deleted => resolve({
                     message: `Finished cleaning  **${deleted}** bot messages in last **${args}** message(s) of ${msg.channel.mention}, **${msg.author.username}**-senpai.`,
                     delete: true
