b44fb51fee6d0a518e45d055a34276e226ecb7d1
hsiw <xanizl@me.com>
2016-10-14 21:52:16 -0400

    Fixed the 'help' command to use the new command system

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -1,21 +1,27 @@
 let utils = require('./../../utils/utils.js');
 
 module.exports = {
-    process: (bot, msg, suffix) => {
-        if (commands.hasOwnProperty(suffix)&& suffix !== 'help') bot.createMessage(msg.channel.id, commands[suffix].help());
-        else {
-            let help = {};
-            let helpMsg = `**__${bot.user.username} Commands\n__**`;
-            for (let command in commands) {
-                if (commands[command].type === 'admin' && !admins.includes(msg.author.id)) continue;
-                if (!help.hasOwnProperty(commands[command].type)) help[commands[command].type] = [];
-                if (!commands[command].privateCheck(msg)) help[commands[command].type].push(command);
+    process: (msg, args) => {
+        return new Promise(resolve => {
+            if (commands.hasOwnProperty(args) && args !== 'help') resolve({
+                message: commands[args].help()
+            })
+            else {
+                let help = {},
+                    helpMsg = `**__${bot.user.username} Commands\n__**`;
+                for (let command in commands) {
+                    if (commands[command].type === 'admin' && !admins.includes(msg.author.id)) continue;
+                    if (!help.hasOwnProperty(commands[command].type)) help[commands[command].type] = [];
+                    if (!commands[command].privateCheck(msg)) help[commands[command].type].push(command);
+                }
+                help = utils.sortObj(help);
+                for (let type in help) {
+                    helpMsg += `\n**${utils.toTitleCase(type)}:** ` + help[type].sort().map(cmd => '`' + cmd + '`').join(", ");
+                }
+                resolve({
+                    message: helpMsg + '\n\nFor more info on these commands use `help [command]`'
+                });
             }
-            help = utils.sortObj(help);
-            for (let type in help) {
-                helpMsg += `\n**${utils.toTitleCase(type)}:** ` + help[type].sort().map(cmd => '`' + cmd + '`').join(", ");
-            }
-            bot.createMessage(msg.channel.id, helpMsg+'\n\nFor more info on these commands use `help [command]`').catch();
-        }
+        });
     }
 }
