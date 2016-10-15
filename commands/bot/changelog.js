98e2ed203d4ef7833c0b7a43500e1b685ae8267d
hsiw <xanizl@me.com>
2016-10-15 15:06:47 -0400

    Updated changelog to new system
    Removed extra () from invite

--- commands/bot/changelog.js
+++ commands/bot/changelog.js
@@ -1,11 +1,14 @@
 module.exports = {
     usage: 'Returns the last changelog for the bot.',
-    delete: true,
+    delete: false,
     cooldown: 10,
-    process: (bot, msg) => {
-        bot.getMessages('143904176613752832', 1).then(message => {
-            let msgString = '__**Changelog**__\n\n' + message[0].content;
-            bot.getDMChannel(msg.author.id).then(privateChannel => bot.createMessage(privateChannel.id, msgString))
-        }).catch()
+    process: (msg, args, bot) => {
+        return new Promise(resolve => {
+            bot.getMessages('143904176613752832', 1).then(message => {
+                resolve({
+                    message: message[0].content
+                });
+            })
+        });
     }
 }
--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -2,7 +2,7 @@ module.exports = {
     usage: 'Returns a link to invite this bot to your server.',
     delete: false,
     cooldown: 20,
-    process: (msg) => {
+    process: msg => {
         return Promise.resolve({
             message: `__**The following link may be used to invite me to your server:**__
 **<https://discordapp.com/oauth2/authorize?&client_id=161620224305528833&scope=bot&permissions=67365888>**`
