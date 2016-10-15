7bc35c2dfe97d2d514dc7b1e6f041dec06745a0f
hsiw <xanizl@me.com>
2016-10-15 09:31:06 -0400

    Removed source command in favor of info

--- commands/bot/source.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    usage: 'Returns a link to the source code for this bot.',
-    cooldown: 30,
-    process: (bot, msg) => {
-        bot.createMessage(msg.channel.id, '__**The source code for this bot can be found here:**__\n**<https://github.com/hsiw/Wishbot>**').catch();
-    }
-}
