b3050a8a2d96de76bb0fbdb4447ce42fa3df9ccf
hsiw <xanizl@me.com>
2016-10-16 11:36:57 -0400

    Commented whois
    Added about alias to whois

--- commands/bot/whois.js
+++ commands/bot/whois.js
@@ -1,16 +1,20 @@
-let getName = require('./../../utils/utils.js').getName, 
-    moment = require('moment'); 
- 
-module.exports = { 
-    usage: "Gives info on the user, can take a username/nickname(can take a mention if a match isn't found) to find the info of that user.\n`info [none] or [user]`", 
-    dm: false, 
-    delete: false, 
-    cooldown: 5, 
-    process: (msg, args) => { 
-        return new Promise(resolve => { 
-            let = user = msg.mentions.length === 1 ? msg.channel.guild.members.get(msg.mentions[0].id) : getName(msg, args); 
-            if (user) { 
-                resolve({ 
+let getName = require('./../../utils/utils.js').getName,
+    moment = require('moment');
+
+module.exports = {
+    usage: "Gives info on the user, can take a username/nickname(can take a mention if a match isn't found) to find the info of that user.\n`info [none] or [user]`",
+    aliases: ['about'],
+    dm: false,
+    delete: false,
+    cooldown: 5,
+    process: (msg, args) => {
+        return new Promise(resolve => {
+            //Get user using either the mention or using the getName function
+            let = user = msg.mentions.length === 1 ? msg.channel.guild.members.get(msg.mentions[0].id) : getName(msg, args);
+            //This should always fire 
+            if (user) {
+                //Resolves will info about the user using moment to format the date and times properly as well as seeing how long ago stuff was
+                resolve({
                     message: ` 
 \`\`\`Swift 
 User: ${user.user.username}#${user.user.discriminator} 
@@ -20,12 +24,13 @@ ${user.game !== null ? 'Playing: \''+user.game.name+'\'\n' : ''}Join Date: ${mom
 Creation Date: ${moment(user.user.createdAt).utc().format('ddd MMM DD YYYY | kk:mm:ss')} (${moment(user.user.createdAt).fromNow()}) 
 Avatar URL: \"${user.user.avatarURL}\" 
 \`\`\` 
-                                ` 
-                }); 
-            } else resolve({ 
-                message: args + " is not a valid user.", 
-                delete: true 
-            }) 
-        }); 
-    } 
+                                `
+                });
+            } //This is just here incase something goes wrong 
+            else resolve({
+                message: args + " is not a valid user.",
+                delete: true
+            })
+        });
+    }
 }
