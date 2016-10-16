a3b90bdef3aef86373bc47ed58649369600b14c6
hsiw <xanizl@me.com>
2016-10-15 22:00:12 -0400

    Added cleverbot to open source commands

---
+++ commands/cleverbot/chat.js
@@ -0,0 +1,43 @@
+let Cleverbot = require('cleverbot-node'),
+    onee = new Cleverbot,
+    decode = require('entities');
+
+Cleverbot.prepare(() => {});
+
+module.exports = {
+    usage: "Chat with this bot using the Cleverbot API\n`chat [text]` or `@BotMention [text]`",
+    delete: false,
+    cooldown: 2,
+    process: msg => {
+        return new Promise(resolve => {
+            let text = (msg.cleanContent.split(' ').length > 1) ? msg.cleanContent.substring(msg.cleanContent.indexOf(' ') + 1).replace('@', '') : 'Hi';
+            msg.channel.sendTyping();
+            Cleverbot.prepare(() => {
+                onee.write(text, response => {
+                    if (!response.message) {
+                        resolve({
+                            message: "Cleverbot is currently reseting. Please try again in a moment",
+                            delete: true
+                        });
+                        delete require.cache[require.resolve('cleverbot-node')];
+                        Cleverbot = require('cleverbot-node');
+                        onee = new Cleverbot();
+                        console.log("Cleverbot was reset because nothing was returned");
+                    } else {
+                        response = unicodeToChar(response.message.replace(/<br \/>/g, " ").replace(/\r?\n|\r/g, "\n").replace(/\[(i|\/i)\]/g, "*").replace(/\[(b|\/b)\]/g, "**").replace(/\|/g, "\\u"))
+                        resolve({
+                            message: "🗨 - " + decode.decodeHTML(response)
+                        })
+                    }
+                });
+            });
+        });
+    }
+}
+
+function unicodeToChar(text) {
+    return text.replace(/\\u[\dA-F]{4}/gi,
+        function(match) {
+            return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+        });
+}
