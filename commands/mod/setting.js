103f1b9fb9f530f65bed3167e43e6b096e2e36fe
hsiw <xanizl@me.com>
2016-10-19 10:00:05 -0400

    Fixed setting not returning a promise
    Fully commented setting

--- commands/mod/setting.js
+++ commands/mod/setting.js
@@ -20,24 +20,29 @@ __Welcome/Leave Messages can include the following text to return different thin
     togglable: false,
     cooldown: 5,
     process: (msg, args) => {
-        if (args.toLowerCase() === 'tableflip') Database.toggleSetting(msg.channel.guild.id, args).then(result => resolve({
-            message: "⚙ " + result + " ⚙",
-            delete: true
-        }))
-        else if (args.toLowerCase().startsWith('welcome') || args.toLowerCase().startsWith('leave')) {
-            let setting = args.split(' ')[0],
-                message = args.substring(setting.length + 1, args.length);
-            if (message.length > 256) resolve({
-                message: "🚫 Welcome/Leave Messages are limited to 256 characters in length. 🚫",
-                delete: true
-            })
-            else Database.toggleSetting(msg.channel.guild.id, setting, message, msg.channel.id).then(result => resolve({
+        return new Promise(resolve => {
+            //If args are tableflip then toggle the tableflip setting
+            if (args.toLowerCase() === 'tableflip') Database.toggleSetting(msg.channel.guild.id, args).then(result => resolve({
                 message: "⚙ " + result + " ⚙",
                 delete: true
             }))
-        } else resolve({
-            message: "🚫 `" + args.split(' ')[0] + "` isn't an available setting 🚫",
-            delete: true
+            else if (args.toLowerCase().startsWith('welcome') || args.toLowerCase().startsWith('leave')) {
+                let setting = args.split(' ')[0], //The setting to be changed(either welcome or leave)
+                    message = args.substring(setting.length + 1, args.length); //Create a string of the message without the extra space
+                if (message.length > 256) //Messages can only be 256 characters in length due to database reasons
+                    resolve({
+                    message: "🚫 Welcome/Leave Messages are limited to 256 characters in length. 🚫",
+                    delete: true
+                })
+                else Database.toggleSetting(msg.channel.guild.id, setting, message, msg.channel.id).then(result => resolve({ //Toggles the setting, if no message passed toggles it off otherwise sets the message for the current channel
+                    message: "⚙ " + result + " ⚙",
+                    delete: true
+                }))
+            } else //If neither tableflip, welcome or leave
+                resolve({
+                message: "🚫 `" + args.split(' ')[0] + "` isn't an available setting 🚫",
+                delete: true
+            })
         })
     }
 }
