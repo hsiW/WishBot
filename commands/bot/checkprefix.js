1eab15f30ee9a85e3c823520bfe22966d38204a3
hsiw <xanizl@me.com>
2016-11-05 16:51:02 -0400

    Commented checkprefix
    Prevented from being used in DM's

--- commands/bot/checkprefix.js
+++ commands/bot/checkprefix.js
@@ -3,12 +3,14 @@ const Database = require('./../../utils/database.js'),
 
 module.exports = {
     usage: 'Returns the current prefix for the guild. Overrides current command prefix and will always work with the default.',
+    dm: false,
     delete: false,
     cooldown: 10,
     process: (msg, args) => {
         return new Promise(resolve => {
+            //Returns the current prefix for the guild by checking if its in the prefix database and returning the entry if so, otherwise returning the default prefix.
             resolve({
-                message: `The current command prefix for this guild is: \`${msg.channel.guild && Database.getPrefix(msg.channel.guild.id) !== undefined ? Database.getPrefix(msg.channel.guild.id) : options.prefix}\``
+                message: `The current command prefix for this guild is: \`${Database.getPrefix(msg.channel.guild.id) !== undefined ? Database.getPrefix(msg.channel.guild.id) : options.prefix}\``
             });
         });
     }
