78f681f96633f8527906d96cf7fcbd4d0bbc7d54
hsiw <xanizl@me.com>
2016-11-07 09:31:36 -0500

    Changed usage for stats to tell more about what it returns

--- commands/bot/stats.js
+++ commands/bot/stats.js
@@ -1,7 +1,7 @@
 const serverUptime = require('os').uptime();
 
 module.exports = {
-    usage: "Returns some basic stats for the bot.",
+    usage: "Returns stats for the bot. Includes bot/process/server uptime, memory usage, number of shards, channels/privateChannels/guilds/users available, and command usage.",
     delete: false,
     cooldown: 20,
     process: (msg, args, bot) => {
