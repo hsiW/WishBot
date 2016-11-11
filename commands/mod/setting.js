ea770dae1065a4a13d20f7996d526dbbf98e12ab
hsiw <xanizl@me.com>
2016-11-11 14:11:01 -0500

    Re-formating help for setting

--- commands/mod/setting.js
+++ commands/mod/setting.js
@@ -1,13 +1,14 @@
 const Database = require('./../../utils/database.js');
 
 module.exports = {
-    usage: `Used to configure/toggle settings including **automated table unflipping** and **welcome/leave messages**. **Welcome/leave messages** are sent in the **channel** in which this command is last used and can be toggled off by leaving the message field **blank**.
+    usage: `Used to configure/toggle settings including **automated table unflipping** and **welcome/leave messages**, both of which are off by default. **Welcome/leave messages** are sent in the **channel** in which this command is last used and can be toggled off by leaving the message field **blank**.
+\`\`\`markdown
+# Welcome/Leave Messages can include the following text:
 
-**__Welcome/Leave Messages can include the following text:__**\`\`\`markdown
-[Text](Replaced with this) #Example
+[Example](Replaced with this)
 [GuildName](Guild Name)
-[ChannelMention](Mention to Current Channel)
-[ChannelName](Name of the Current Channel)
+[ChannelMention](Mention to Channel)
+[ChannelName](Name of the Channel)
 [UserName](Name of the User)
 [UserMention](Mention to the User)
 
@@ -23,10 +24,11 @@ module.exports = {
     process: (msg, args) => {
         return new Promise(resolve => {
             //If args are tableflip then toggle the tableflip setting
-            if (args.toLowerCase() === 'tableflip') Database.toggleSetting(msg.channel.guild.id, args).then(result => resolve({
-                message: "⚙ " + result + " ⚙",
-                delete: true
-            }))
+            if (args.toLowerCase() === 'tableflip') Database.toggleSetting(msg.channel.guild.id, args).then(result =>
+                resolve({
+                    message: "⚙ " + result + " ⚙",
+                    delete: true
+                }))
             else if (args.toLowerCase().startsWith('welcome') || args.toLowerCase().startsWith('leave')) {
                 let setting = args.split(' ')[0], //The setting to be changed(either welcome or leave)
                     message = args.substring(setting.length + 1, args.length); //Create a string of the message without the extra space
@@ -35,10 +37,12 @@ module.exports = {
                     message: "🚫 Welcome/Leave Messages are limited to 256 characters in length. 🚫",
                     delete: true
                 })
-                else Database.toggleSetting(msg.channel.guild.id, setting, message, msg.channel.id).then(result => resolve({ //Toggles the setting, if no message passed toggles it off otherwise sets the message for the current channel
-                    message: "⚙ " + result + " ⚙",
-                    delete: true
-                }))
+                else Database.toggleSetting(msg.channel.guild.id, setting, message, msg.channel.id).then(result =>
+                    //Toggles the setting, if no message passed toggles it off otherwise sets the message for the current channel
+                    resolve({
+                        message: "⚙ " + result + " ⚙",
+                        delete: true
+                    }))
             } else //If neither tableflip, welcome or leave
                 resolve({
                 message: "🚫 `" + args.split(' ')[0] + "` isn't an available setting 🚫",
