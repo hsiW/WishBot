54a3ca42c898294a7d32bb431e54f4501b4c0c9b
hsiw <xanizl@me.com>
2016-11-17 19:09:38 -0500

    Fixed purge not taking into account before the sent message

--- commands/bot/clean.js
+++ commands/bot/clean.js
@@ -9,11 +9,11 @@ module.exports = {
             //Check to make sure the bot has access to the bulk delete endpoint which requires mangeMessages
             if (msg.channel.permissionsOf(bot.user.id).has('manageMessages')) {
                 //Purges the bots messages from the number of messages the user requested
-                msg.channel.purge(args, message => message.author.id === bot.user.id).then(deleted => resolve({
+                msg.channel.purge(args, message => message.author.id === bot.user.id, msg.id).then(deleted => resolve({
                     //When purge is finished return the relevant message which will be deleted after 5s 
                     message: `Finished cleaning  **${deleted}** bot messages in last **${args}** message(s) of ${msg.channel.mention}, **${msg.author.username}**-senpai.`,
                     delete: true
-                })).catch(err=> console.log(errorC(err)))
+                })).catch(err => console.log(errorC(err)))
             } else {
                 var deleted = 0; //Tracks the number of deleted messages
                 //Get an array of the number of messages starting before the command message
