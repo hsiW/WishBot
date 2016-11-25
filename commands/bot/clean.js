7428c4ab0fdba2bf11251f2024cfe6994cf8ac55
hsiw <xanizl@me.com>
2016-11-25 16:42:43 -0500

    Fixed clean in dm's

--- commands/bot/clean.js
+++ commands/bot/clean.js
@@ -7,7 +7,7 @@ module.exports = {
         return new Promise(resolve => {
             /^\d+$/.test(args) ? args = parseInt(args) : args = 50; //Checks if args is a number(as well as it existing) and if so sets args to that number otherwise defaults to 50
             //Check to make sure the bot has access to the bulk delete endpoint which requires mangeMessages
-            if (msg.channel.permissionsOf(bot.user.id).has('manageMessages')) {
+            if (msg.channel.guild && msg.channel.permissionsOf(bot.user.id).has('manageMessages')) {
                 //Purges the bots messages from the number of messages the user requested
                 msg.channel.purge(args, message => message.author.id === bot.user.id, msg.id).then(deleted => resolve({
                     //When purge is finished return the relevant message which will be deleted after 5s 
