c671266353d2f1458b5df9ae64f508845f9bf4ef
hsiw <xanizl@me.com>
2016-11-27 09:16:38 -0500

    Cleaned up command logging slightly

--- utils/commandHandler.js
+++ utils/commandHandler.js
@@ -9,19 +9,20 @@ module.exports = (msg, args, cmd, bot) => {
         //Check if the used location passes the private check or if it passes the DM check to prevent some commands from being used in unintended locations
         if (cmd.privateCheck(msg) || (!cmd.dm && !msg.channel.guild)) return;
         //Cooldown check, admins ignore cooldowns
-        else if (!(admins.indexOf(msg.author.id) > -1) && cmd.cooldownCheck(msg.author.id)) bot.createMessage(msg.channel.id, `\`${cmd.name}\` is currently on cooldown for ${cmd.cooldownTime(msg.author.id).toFixed(1)}s`);
+        else if (!(admins.indexOf(msg.author.id) > -1) && cmd.cooldownCheck(msg.author.id))
+            bot.createMessage(msg.channel.id, `\`${cmd.name}\` is currently on cooldown for ${cmd.cooldownTime(msg.author.id).toFixed(1)}s`);
         //Process the command
         else {
             //Execute the command
             cmd.exec(msg, args, bot)
             if (msg.channel.guild) {
                 //Command Logging in Guilds
-                console.log(guildC("@" + msg.channel.guild.name + ":") + channelC(" #" + msg.channel.name) + ": " + warningC(cmd.name) + " was used by " + userC(msg.author.username));
+                console.log(`${guildC("@" + msg.channel.guild.name + ":")}${channelC(" #" + msg.channel.name)}: ${warningC(cmd.name)} was used by ${userC(msg.author.username)}`);
                 //Updates the timestamp for the guild to mark it as active
                 usageChecker.updateTimestamp(msg.channel.guild);
             }
             //Comand Logging in PM's
-            else console.log(guildC("@Private Message: ") + warningC(cmd.name) + " was used by " + userC(msg.author.username));
+            else console.log(`${guildC("@Private Message:")} ${warningC(cmd.name)} was used by ${userC(msg.author.username)}`);
         }
     }
 }
